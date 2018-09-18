b64cd7d3b020a24805a0e15ed96a82b73eb323e2
sergioruiz <4043216+sergioruiz@users.noreply.github.com>
2018-09-18 11:50:15 -0500

    Operators in Python
    Operators and precedence

---
+++ Operators.py
@@ -0,0 +1,15 @@
+n = 5 + 12 / 2 * -4
+# n = 5 + 6 * -4
+# n = 5 + -24
+# n = 19
+print(n)
+# Python implements "Precedence"
+# **
+# -x, +x
+# *, /, //, %
+# +, -
+# <, <=, >, >=, !=, ==
+# not
+# and
+# or
+
